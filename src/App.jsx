84f8a663982c0b12890f471d55567a6ee0f78c30
ahmadfrazz <ahmedfaraz12580@gmail.com>
2024-11-24 20:27:39 +0500

    dynamic imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,35 @@
+import React, { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
-import PageNotFound from "./Pages/PageNotFound";
-import Dashboard from "./Pages/Dashboard/Dashboard.jsx";
-import Layout from "./Components/DashboardWrapper/Layout";
-import Notes from "./Pages/Notes/Notes.jsx";
+import { Spin } from "antd";
+
+const PageNotFound = React.lazy(() => import("./Pages/PageNotFound"));
+const Dashboard = React.lazy(() => import("./Pages/Dashboard/Dashboard.jsx"));
+const Layout = React.lazy(() => import("./Components/DashboardWrapper/Layout"));
+const Notes = React.lazy(() => import("./Pages/Notes/Notes.jsx"));
 
 function App() {
   return (
     <>
-      <Routes>
-        {/* === Public Routes === */}
-        <Route path="/" element={<Layout />}>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="notes" element={<Notes />} />
-        </Route>
+      <Suspense
+        fallback={
+          <Spin
+            className="h-screen flex place-items-center justify-center"
+            size="large"
+          />
+        }
+      >
+        <Routes>
+          {/* === Public Routes === */}
+          <Route path="/" element={<Layout />}>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="notes" element={<Notes />} />
+          </Route>
 
-        {/* === Unknown Routes === */}
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
+          {/* === Unknown Routes === */}
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </Suspense>
     </>
   );
 }
