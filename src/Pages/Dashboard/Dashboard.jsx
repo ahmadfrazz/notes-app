2230c18c04b6fb65ac54e277c5f3a0e43f5ea442
ahmadfrazz <ahmedfaraz12580@gmail.com>
2024-11-24 18:45:04 +0500

    dashboard fix

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -10,7 +10,7 @@ function Dashboard() {
   };
   return (
     <>
-      <div className="w-full rounded-[4px] boxshadow1 shadow-custom bg-[#FFFFFF] h-[calc(100vh-100px)] px-[64px] py-[44px]">
+      <div className="w-full rounded-[4px] boxshadow1 shadow-custom bg-[#FFFFFF] h-[calc(100vh-100px)] px-[10px] py-[44px] sm:px-[64px] sm:py-[44px] flex justify-center sm:block">
         <Button text="Open Child Document" onButtonClick={onButtonClick} />
       </div>
     </>
